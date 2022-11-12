3a3d77a37f5f12fe4a26b14bd7241dfea6a17aca
klarahanany <116444303+klarahanany@users.noreply.github.com>
2022-11-12 11:23:23 +0200

    class

--- knight'sMove/src/Model/Game.java
+++ knight'sMove/src/Model/Game.java
@@ -1,5 +1,45 @@
 package Model;
 
+import java.sql.Time;
+
+import Enum.GameStatus;
+
 public class Game {
+	private Board board;
+	private Player player;
+	private Queen queen;
+	private King king;
+	private GameStatus gameStatus;
+	private Time time;
+
 	
+	public void initiateGame() {
+		
+	}
+	public void startGame() {
+		
+	}
+	public void stopGame() {
+		
+	}
+	public void finishGame() {
+		
+	}
+	public void pauseGame() {
+		
+	}
+	public void resumeGame() {
+		
+	}
+	public void updatePoints() {
+		
+	}
+
+	
+	
+	
+	
+	public void popQuestion() {
+		
+	}
 }
--- knight'sMove/src/Model/SysData.java
+++ knight'sMove/src/Model/SysData.java
@@ -6,4 +6,31 @@ public class SysData {
 	private HashMap<Integer, Game> games;
 	private HashMap<Integer, Question> questions;
 	
+	public void Initiate() {
+		
+	}
+	public void	loadData() {
+		
+	}
+	public void	writeData(){
+		
+	}
+	public void	loadQuestions(){
+		
+	}
+	public void	writeQuestions()  {
+		
+	}
+	public void	updateQuesthion() {
+		
+	}
+	public void	addQuestion() {
+		
+	}
+	public void	removeQuestion () {
+		
+	}
+	public void addGame(){
+		
+	}
 }
